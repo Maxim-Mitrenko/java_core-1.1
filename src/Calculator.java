c9c84cc9f2222293e13f8204558689bfee3927e1
Maxim-Mitrenko <klevaya.farm@gmail.com>
2022-02-21 11:55:38 +0300

    Fix division on 0

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,9 +6,13 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> division = (x, y) -> x / y;
+    BinaryOperator<Integer> division = (x, y) -> {
+        if (y == 0) throw new NullDivisionException(x);
+        return x / y;
+    };
+
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x < 0 ? x * -1 : x;
     Predicate<Integer> isPositive = x -> x > 0;
-    Consumer<Integer> println= System.out::println;
+    Consumer<Integer> println = System.out::println;
 }
